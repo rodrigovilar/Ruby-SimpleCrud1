afaf23b30783dac9013f0118ee0f556610e13c67
Rodrigo Vilar <rvilar@gmail.com>
2015-04-17 10:42:10 -0300

    Two similar entities

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150416204637) do
+ActiveRecord::Schema.define(version: 20150417133528) do
 
   create_table "clientes", force: :cascade do |t|
     t.string   "nome"
     t.integer  "idade"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "produtos", force: :cascade do |t|
+    t.string   "nome"
+    t.integer  "quantidade"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
